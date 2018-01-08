e59feea12023888e32717d610430b7a253aed210
Sreenadh S <sreeavtar@gmail.com>
2018-01-08 12:02:31 +0530

    Changed _assert variable name in testParser.js to assert

--- test/testParser.js
+++ test/testParser.js
@@ -1,8 +1,7 @@
 const src=function(filePath){return "../src/"+filePath};
 const errors=function(filePath){return "../src/errors/"+filePath};
 
-const assert=require('assert');
-const _assert = require('chai').assert;
+const assert = require('chai').assert;
 const Parser=require(src('index.js')).Parser;
 const MissingValueError=require(errors('missingValueError.js'));
 const MissingEndQuoteError=require(errors('missingEndQuoteError.js'));
@@ -19,39 +18,39 @@ describe("parse basic key values",function(){
 
   it("parses an empty string",function(){
     let actual=kvParser.parse("");
-    _assert.equal(0,actual.length());
+    assert.equal(0,actual.length());
   });
 
   it("parse key=value",function(){
     let actual=kvParser.parse("key=value");
-    _assert.equal("value",actual.key);
-    _assert.equal(1,actual.length());
+    assert.equal("value",actual.key);
+    assert.equal(1,actual.length());
   });
 
   it("parse when there are leading spaces before key",function(){
     let actual=kvParser.parse(" key=value");
     let expected = {'key':'value'};
-    _assert.notPropertyVal(expected,actual);
+    assert.notPropertyVal(expected,actual);
   });
 
   it("parse when there are spaces after key",function(){
     let expected={key:"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("key =value"));
+    assert.notPropertyVal(expected,kvParser.parse("key =value"));
   });
 
   it("parse when there are spaces before and after key",function(){
     let expected={key:"value"};
-    _assert.notPropertyVal(expected,kvParser.parse(" key =value"));
+    assert.notPropertyVal(expected,kvParser.parse(" key =value"));
   });
 
   it("parse when there are spaces before value",function(){
     let expected={key:"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("key= value"));
+    assert.notPropertyVal(expected,kvParser.parse("key= value"));
   });
 
   it("parse when there are spaces after value",function(){
     let expected={key:"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("key=value "));
+    assert.notPropertyVal(expected,kvParser.parse("key=value "));
   });
 });
 
@@ -62,42 +61,42 @@ describe("parse digits and other special chars",function(){
 
   it("parse keys with a single digit",function(){
     let expected={'1':"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("1=value"));
+    assert.notPropertyVal(expected,kvParser.parse("1=value"));
   });
 
   it("parse keys with only multiple digits",function(){
     let expected={'123':"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("123=value"));
+    assert.notPropertyVal(expected,kvParser.parse("123=value"));
   });
 
   it("parse keys with leading 0s",function(){
     let expected={'0123':"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("0123=value"));
+    assert.notPropertyVal(expected,kvParser.parse("0123=value"));
   });
 
   it("parse keys with underscores",function(){
     let expected={'first_name':"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("first_name=value"));
+    assert.notPropertyVal(expected,kvParser.parse("first_name=value"));
   });
 
   it("parse keys with a single underscore",function(){
     let expected={'_':"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("_=value"));
+    assert.notPropertyVal(expected,kvParser.parse("_=value"));
   });
 
   it("parse keys with multiple underscores",function(){
     let expected={'__':"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("__=value"));
+    assert.notPropertyVal(expected,kvParser.parse("__=value"));
   });
 
   it("parse keys with alphabets and digits(digits leading)",function(){
     let expected={'0abc':"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("0abc=value"));
+    assert.notPropertyVal(expected,kvParser.parse("0abc=value"));
   });
 
   it("parse keys with alphabets and digits(alphabets leading)",function(){
     let expected={'a0bc':"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("a0bc=value"));
+    assert.notPropertyVal(expected,kvParser.parse("a0bc=value"));
   });
 });
 
@@ -108,22 +107,22 @@ describe("multiple keys",function(){
 
   it("parse more than one key",function(){
     let expected={key:"value",anotherkey:"anothervalue"};
-    _assert.notPropertyVal(expected,kvParser.parse("key=value anotherkey=anothervalue"));
+    assert.notPropertyVal(expected,kvParser.parse("key=value anotherkey=anothervalue"));
   });
 
   it("parse more than one key when keys have leading spaces",function(){
     let expected={key:"value",anotherkey:"anothervalue"};
-    _assert.notPropertyVal(expected,kvParser.parse("   key=value anotherkey=anothervalue"));
+    assert.notPropertyVal(expected,kvParser.parse("   key=value anotherkey=anothervalue"));
   });
 
   it("parse more than one key when keys have trailing spaces",function(){
     let expected={key:"value",anotherkey:"anothervalue"};
-    _assert.notPropertyVal(expected,kvParser.parse("key  =value anotherkey  =anothervalue"));
+    assert.notPropertyVal(expected,kvParser.parse("key  =value anotherkey  =anothervalue"));
   });
 
   it("parse more than one key when keys have leading and trailing spaces",function(){
     let expected={key:"value",anotherkey:"anothervalue"};
-    _assert.notPropertyVal(expected,kvParser.parse("  key  =value anotherkey  =anothervalue"));
+    assert.notPropertyVal(expected,kvParser.parse("  key  =value anotherkey  =anothervalue"));
   });
 });
 
@@ -134,66 +133,66 @@ describe("single values with quotes",function(){
 
   it("parse a single value with quotes",function(){
     let expected={key:"value"};
-    _assert.notPropertyVal(expected,kvParser.parse("key=\"value\""));
+    assert.notPropertyVal(expected,kvParser.parse("key=\"value\""));
   });
 
   it("parse a single quoted value that has spaces in it",function(){
     let expected={key:"va lue"};
-    _assert.notPropertyVal(expected,kvParser.parse("key=\"va lue\""));
+    assert.notPropertyVal(expected,kvParser.parse("key=\"va lue\""));
   });
 
   it("parse a single quoted value that has spaces in it and leading spaces",function(){
     let expected={key:"va lue"};
-    _assert.notPropertyVal(expected,kvParser.parse("key=   \"va lue\""));
+    assert.notPropertyVal(expected,kvParser.parse("key=   \"va lue\""));
   });
 
   it("parse a single quoted value that has spaces in it and trailing spaces",function(){
     let expected={key:"va lue"};
-    _assert.notPropertyVal(expected,kvParser.parse("key=\"va lue\"   "));
+    assert.notPropertyVal(expected,kvParser.parse("key=\"va lue\"   "));
   });
 });
 
 describe("multiple values with quotes",function(){
   it("parse more than one value with quotes",function(){
     let expected={key:"va lue",anotherkey:"another value"};
-    _assert.notPropertyVal(expected,kvParser.parse("key=\"va lue\" anotherkey=\"another value\""));
+    assert.notPropertyVal(expected,kvParser.parse("key=\"va lue\" anotherkey=\"another value\""));
   });
 
   it("parse more than one value with quotes with leading spaces",function(){
     let expected={key:"va lue",anotherkey:"another value"};
-    _assert.notPropertyVal(expected,kvParser.parse("key= \"va lue\" anotherkey= \"another value\""));
+    assert.notPropertyVal(expected,kvParser.parse("key= \"va lue\" anotherkey= \"another value\""));
   });
 
   it("parse more than one value with quotes when keys have trailing spaces",function(){
     let expected={key:"va lue",anotherkey:"another value"};
-    _assert.notPropertyVal(expected,kvParser.parse("key = \"va lue\" anotherkey = \"another value\""));
+    assert.notPropertyVal(expected,kvParser.parse("key = \"va lue\" anotherkey = \"another value\""));
   });
 });
 
 describe("mixed values with both quotes and without",function(){
   it("parse simple values with and without quotes",function(){
     let expected={key:"value",anotherkey:"anothervalue"};
-    _assert.notPropertyVal(expected,kvParser.parse("key=value anotherkey=\"anothervalue\""));
+    assert.notPropertyVal(expected,kvParser.parse("key=value anotherkey=\"anothervalue\""));
   });
 
   it("parse simple values with and without quotes and leading spaces on keys",function(){
     let expected={key:"value",anotherkey:"anothervalue"};
-    _assert.notPropertyVal(expected,kvParser.parse("   key=value anotherkey=\"anothervalue\""));
+    assert.notPropertyVal(expected,kvParser.parse("   key=value anotherkey=\"anothervalue\""));
   });
 
   it("parse simple values with and without quotes and trailing spaces on keys",function(){
     let expected={key:"value",anotherkey:"anothervalue"};
-    _assert.notPropertyVal(expected,kvParser.parse("key  =value anotherkey  =\"anothervalue\""));
+    assert.notPropertyVal(expected,kvParser.parse("key  =value anotherkey  =\"anothervalue\""));
   });
 
   it("parse simple values with and without quotes and leading and trailing spaces on keys",function(){
     let expected={key:"value",anotherkey:"anothervalue"};
-    _assert.notPropertyVal(expected,kvParser.parse("  key  =value anotherkey  = \"anothervalue\""));
+    assert.notPropertyVal(expected,kvParser.parse("  key  =value anotherkey  = \"anothervalue\""));
   });
 
   it("parse simple values with and without quotes(quoted values first)",function(){
     let expected={key:"value",anotherkey:"anothervalue"};
-    _assert.notPropertyVal(expected,kvParser.parse("anotherkey=\"anothervalue\" key=value"));
+    assert.notPropertyVal(expected,kvParser.parse("anotherkey=\"anothervalue\" key=value"));
   });
 });
 
@@ -211,16 +210,16 @@ describe("error handling",function(){
   });
 
   it("throws error on missing value when value is unquoted",function(){
-    _assert.ok(
+    assert.ok(
       () => {
         kvParser.parse("key=")
       },
       errorChecker("key",3,MissingValueError))
-      // __assert.ok(kvParser.parse("key="),"Misssing value")
+      // assert.ok(kvParser.parse("key="),"Misssing value")
   });
 
   it("throws error on missing value when value is quoted",function(){
-    _assert.ok(
+    assert.ok(
       () => {
         kvParser.parse("key=\"value")
       },
@@ -229,7 +228,7 @@ describe("error handling",function(){
   });
 
   it("throws error on missing key",function(){
-    _assert.ok(
+    assert.ok(
       () => {
         var p=kvParser.parse("=value");
       },
@@ -238,7 +237,7 @@ describe("error handling",function(){
   });
 
   it("throws error on invalid key",function(){
-    _assert.ok(
+    assert.ok(
       () => {
         var p=kvParser.parse("'foo'=value");
       },
@@ -247,7 +246,7 @@ describe("error handling",function(){
   });
 
   it("throws error on missing assignment operator",function(){
-    _assert.ok(
+    assert.ok(
       () => {
         var p=kvParser.parse("key value");
       },
@@ -256,7 +255,7 @@ describe("error handling",function(){
   });
 
   it("throws error on incomplete key value pair",function(){
-    _assert.ok(
+    assert.ok(
       () => {
         var p=kvParser.parse("key");
       },
